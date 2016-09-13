2f4d2d2eca4a2d4121bdd4237af9156f18bb3934
adam-tylr <tyler.147@osu.edu>
2016-09-13 18:39:48 -0400

    Created blank Xcode Project

---
+++ Drones on Ice Mobile/Pods/Target Support Files/Pods-Drones on Ice Mobile/Pods-Drones on Ice Mobile-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Drones_on_Ice_Mobile : NSObject
+@end
+@implementation PodsDummy_Pods_Drones_on_Ice_Mobile
+@end
---
+++ Drones on Ice Mobile/Pods/Target Support Files/Pods-Drones on Ice Mobile/Pods-Drones on Ice Mobile-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Drones_on_Ice_MobileVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Drones_on_Ice_MobileVersionString[];
+
