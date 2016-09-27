30dbad4c535e6c6aad213a6da05a4a9f3c509b17
adam-tylr <tyler.147@osu.edu>
2016-09-26 22:41:36 -0400

    Add apple pay support

---
+++ Drones on Ice Mobile/Drones on Ice Mobile/Drones on Ice Mobile/Address.swift
@@ -0,0 +1,21 @@
+//
+//  Address.swift
+//  Drones on Ice Mobile
+//
+//  Created by Adam Tyler on 9/26/16.
+//  Copyright © 2016 Adam Tyler. All rights reserved.
+//
+
+import Foundation
+
+struct Address {
+    var Street: String?
+    var City: String?
+    var State: String?
+    var Zip: String?
+    var FirstName: String?
+    var LastName: String?
+    
+    init() {
+    }
+}
--- Drones on Ice Mobile/Drones on Ice Mobile/Drones on Ice Mobile/StartOrderViewController.swift
+++ Drones on Ice Mobile/Drones on Ice Mobile/Drones on Ice Mobile/StartOrderViewController.swift
@@ -7,13 +7,27 @@
 //
 
 import UIKit
+import PassKit
 
-class StartOrderViewController: UIViewController {
+class StartOrderViewController: UIViewController  {
 
+    var flavor = ""
+    
+    @IBOutlet weak var message: UILabel!
+    @IBOutlet weak var flavorText: UILabel!
+    @IBOutlet weak var totalText: UILabel!
+    @IBOutlet weak var payButton: UIButton!
+    
+    let SupportedPaymentNetworks = [PKPaymentNetwork.visa, PKPaymentNetwork.masterCard, PKPaymentNetwork.amex, PKPaymentNetwork.discover]
+    let ApplePaySwagMerchantID = "merchant.us.adamtyler.drones"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let defaults = UserDefaults.standard
+        let name = defaults.string(forKey: "fName") as String!
+        message.text = "Hey \(name!)! Hungry?"
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +35,73 @@ class StartOrderViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func pay(_ sender: AnyObject) {
+        let request = PKPaymentRequest()
+
+        request.merchantIdentifier = ApplePaySwagMerchantID
+        request.supportedNetworks = SupportedPaymentNetworks
+        request.merchantCapabilities = PKMerchantCapability.capability3DS
+        request.countryCode = "US"
+        request.currencyCode = "USD"
+        
+        request.paymentSummaryItems = [
+            PKPaymentSummaryItem(label: flavor, amount: 8),
+            PKPaymentSummaryItem(label: "DOI", amount: 8)
+        ]
+        
+        
+        request.requiredShippingAddressFields = PKAddressField.all
+        
+        let applePayController = PKPaymentAuthorizationViewController(paymentRequest: request)
+        self.present(applePayController, animated: true, completion: nil)
+        applePayController.delegate = self
+        
+    
+    }
+
+    
+    @IBAction func vanillaTap(_ sender: AnyObject) {
+        flavor = "Vanilla"
+        flavorText.text = "Flavor: \(flavor)"
+        totalText.text = "Total: $8.00"
+    }
+    @IBAction func chocTap(_ sender: AnyObject) {
+        flavor = "Chocolate"
+        flavorText.text = "Flavor: \(flavor)"
+        totalText.text = "Total: $8.00"
+    }
+    @IBAction func strawberryTap(_ sender: AnyObject) {
+        flavor = "Strawberry"
+        flavorText.text = "Flavor: \(flavor)"
+        totalText.text = "Total: $8.00"
+    }
+    @IBAction func berryTap(_ sender: AnyObject) {
+        flavor = "Berry Crisp"
+        flavorText.text = "Flavor: \(flavor)"
+        totalText.text = "Total: $8.00"
+    }
+    @IBAction func caramelTap(_ sender: AnyObject) {
+        flavor = "Caramel"
+        flavorText.text = "Flavor: \(flavor)"
+        totalText.text = "Total: $8.00"
+    }
+    @IBAction func coffeeTap(_ sender: AnyObject) {
+        flavor = "Coffee"
+        flavorText.text = "Flavor: \(flavor)"
+        totalText.text = "Total: $8.00"
+    }
+    @IBAction func pbTap(_ sender: AnyObject) {
+        flavor = "Peanut Butter"
+        flavorText.text = "Flavor: \(flavor)"
+        totalText.text = "Total: $8.00"
+    }
+    @IBAction func pecanTap(_ sender: AnyObject) {
+        flavor = "Pecan"
+        flavorText.text = "Flavor: \(flavor)"
+        totalText.text = "Total: $8.00"
+    }
+    
+
 
     /*
     // MARK: - Navigation
@@ -32,4 +113,20 @@ class StartOrderViewController: UIViewController {
     }
     */
 
+}
+
+extension StartOrderViewController: PKPaymentAuthorizationViewControllerDelegate {
+    @available(iOS 8.0, *)
+    public func paymentAuthorizationViewController(_ controller: PKPaymentAuthorizationViewController, didAuthorizePayment payment: PKPayment, completion: @escaping (PKPaymentAuthorizationStatus) -> Void) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+
+    @nonobjc func paymentAuthorizationViewController(controller: PKPaymentAuthorizationViewController, didAuthorizePayment payment: PKPayment, completion: ((PKPaymentAuthorizationStatus) -> Void)) {
+        completion(PKPaymentAuthorizationStatus.success)
+    }
+    
+    func paymentAuthorizationViewControllerDidFinish(_ controller: PKPaymentAuthorizationViewController) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
 }
